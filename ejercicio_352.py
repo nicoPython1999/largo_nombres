b8335aec527dc38d4dfc682491970f48880fe9cc
nicoPython1999 <nico.sanchezb@duocuc.cl>
2024-06-21 10:49:27 -0400

    Add files via upload
    Se corrigió el error de definir mal la lista y se añadió la capacidad de reconocer el nombre más pequeño.

--- ejercicio_352.py
+++ ejercicio_352.py
@@ -1,16 +1,11 @@
 
-ListaNombres = ()
+ListaNombres = []
 
 for i in range(0,3,1):
     ListaNombres.append(input("Escriba un nombre: "))
 
-print(f"El nombre más largo es ", max(ListaNombres))
-
-
-
-
-
-
+print(f"El nombre más largo es ", max(ListaNombres, key = len))
+print(f"El nombre más pequeño es ", min(ListaNombres, key = len))
 
 
 
